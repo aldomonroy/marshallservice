a796836184b35ed5a1ca5d2074022bb816947ffb
aldo monro <aldomonroy@yandex.com>
2021-07-30 17:43:14 -0500

    actualizacion de metadatos

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -3,25 +3,23 @@
 $nombre = $_POST['name'];
 $mail = $_POST['email'];
 $telefono = $_POST['subject'];
-$empresa = $_POST['message'];
+$empresa = $_POST['mensaje'];
 
 $header = 'from: '.$mail."\r\n";
 $header .= "X-Mailer:PHP/".phpversion()."\r\n";
 $header .= "Mime-Version:1.0 \r\n";
 $header .= "content-Type:text/plain";
 
-$message = "este message fue enviado por: ".$name."\r\n";
-$message .= "Su e-mail es: ".$mail."\r\n";
-$message .= "Telefono: ".$subject."\r\n";
-$message .= "message: ".$_POST['message']."\r\n";
-$message .= "enviado el: ".date('d/m/Y',time());
+$mensaje = "este mensaje fue enviado por: ".$name."\r\n";
+$mensaje .= "Su e-mail es: ".$mail."\r\n";
+$mensaje .= "Telefono: ".$subject."\r\n";
+$mensaje .= "mensaje: ".$_POST['mensaje']."\r\n";
+$mensaje .= "enviado el: ".date('d/m/Y',time());
 
 
-$para = 'consultas@servitecflhuaraz.com';
-$asunto = 'message de mi web';
+$para = 'serviciomarshall@gmail.com';
+$asunto = 'mensaje de mi web';
 
-if(mail($para,$asunto,utf8_decode($message),$header))
-echo "<script type='text/javascript'>alert('Tu message ha sido enviado exitosamente');</script>";
-echo "<script type='text/javascript'>window.location.href='http://servitecflhuaraz.com';</script>";
-
- ?>
+if(mail($para,$asunto,utf8_decode($mensaje),$header))
+echo "<script type='text/javascript'>alert('Tu mensaje ha sido enviado exitosamente');</script>";
+echo "<script type='text/javascript'>window.location.href='https://linktr.ee/marshallservice';</script>"; ?>
